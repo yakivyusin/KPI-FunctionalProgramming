4fb14cef6a522e1700038f937a91ddf9d4810b74
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-12-26 14:43:59 +0200

    Add Aliases and Units of Measure Samples

---
+++ Lct04-Types-Part1/TypesOverBasicTypes/Aliases.fsx
@@ -0,0 +1,29 @@
+﻿type RealNumber = float
+type AdditionFunction = int -> int -> int
+type CustomerId = int
+type OrderId = int
+
+let x:RealNumber = 3.14
+let f:AdditionFunction = fun x y -> x + y
+
+let processCustomerId (id: CustomerId) = 0
+let processOrderId (id: OrderId) = 1
+
+(*
+    Псевдоніми є лише псевдонімами - весь наступний код чудово компілюється та виконується, не даючи безпеки типів
+*)
+
+// Значення правильних псевдонімів
+let customerId:CustomerId = 10
+let orderId:OrderId = 20
+
+processCustomerId customerId |> ignore
+processOrderId orderId |> ignore
+
+// Значення справжніх типів...
+processCustomerId 10 |> ignore
+processOrderId 20 |> ignore
+
+// Переплутали місцями...
+processCustomerId orderId |> ignore
+processOrderId customerId |> ignore
---
+++ Lct04-Types-Part1/TypesOverBasicTypes/UnitsOfMeasure.fsx
@@ -0,0 +1,30 @@
+﻿[<Measure>] type m
+[<Measure>] type sec
+[<Measure>] type kg
+
+let distance = 1.0<m>
+let time = 2.0<sec>
+
+// Три наступні значення мають одну і ту саму одиницю виміру
+let speed = 1.0<m / sec>
+[<Measure>] type speed = m / sec
+let speed2 = 1.0<speed>
+let speed3 = distance / time
+
+let acceleration = speed / time
+let mass = 5.0<kg>
+let force = mass * speed / time
+
+let wtf = 1<m> + 1<kg>
+let wtf2 = 1<m> + 1
+let ok = 1<m> * 2
+
+let meterTwoTimes (m: float<m>) = m * 2.0
+
+meterTwoTimes distance |> ignore
+meterTwoTimes 2.0
+meterTwoTimes 1<kg>
+
+let addKilometer m = m + 1000<m>
+
+addKilometer 1000<m> |> ignore
