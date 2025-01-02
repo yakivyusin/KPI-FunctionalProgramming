72bb7a7c17ea31764091fa10020c21441a7a5b8a
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-01-02 12:35:21 +0200

    Add Loops and Recursion Samples

---
+++ Lct05-ProgramFlow/Loops/LoopExpressions.fsx
@@ -0,0 +1,61 @@
+﻿for i = 1 to 10 do
+    printf "%i " i
+
+printfn ""
+
+for i = 10 downto 1 do
+    printf "%i " i
+
+// warning
+// for i = 1 to 10 do
+//    i * i
+
+for i = 1 to 10 do
+    i * i |> ignore
+
+let sumNumbers limit =
+    let mutable sum = 0
+    for i = 1 to limit do
+        sum <- sum + i
+    sum
+
+sumNumbers 10
+
+(*
+    for...in буде працювати зі всім, що реалізовує інтерфейс System.Collections.IEnumerable.
+    Це стосується як і нативних F# колекцій, так і колекцій з .NET BCL.
+    Щоб не спойлерити нативні колекції, будемо працювати з List<T> із BCL :)
+*)
+open System.Collections.Generic
+
+let list = List()
+list.Add("Hello")
+list.Add("World")
+
+for item in list do
+    printfn "%s" item
+
+let tuplesList = List()
+tuplesList.Add((1, 2))
+tuplesList.Add((3, 4))
+
+for (first, second) in tuplesList do
+    printfn "%i %i" first second
+
+let sumNumbers1 limit =
+    let mutable sum = 0
+    for i in 1 .. limit do
+        sum <- sum + i
+    sum
+
+sumNumbers1 10
+
+let sumNumbers2 limit =
+    let mutable current = limit
+    let mutable sum = 0
+    while not (current = 0) do
+        sum <- sum + current
+        current <- current - 1
+    sum
+
+sumNumbers2 10
---
+++ Lct05-ProgramFlow/Loops/RecursiveFunctions.fsx
@@ -0,0 +1,60 @@
+﻿// fail:
+//let sumNumbers limit =
+//    match limit with
+//    | 0 -> 0
+//    | x -> x + sumNumbers (x - 1)
+
+let rec sumNumbers limit =
+    match limit with
+    | 0 -> 0
+    | x -> x + sumNumbers (x - 1)
+
+sumNumbers 1 // ok
+sumNumbers 10 // ok
+sumNumbers 100 // ok
+sumNumbers 1000 // а у нас вже stack overflow?
+sumNumbers 10000 // а тепер у нас вже точно stack overflow?
+sumNumbers 100000 // ну тут вже у нас stack overflow?
+sumNumbers 1000000 // а тут у нас stack overflow?
+
+(*
+    Компілятор сам оптимізовує хвостову рекурсію у цикли
+*)
+let tailSumNumbers limit =
+    let rec inner current total =
+        match current with
+        | 0 -> total
+        | x -> inner (current - 1) (current + total)
+    inner limit 0
+
+tailSumNumbers 1
+tailSumNumbers 10
+tailSumNumbers 100
+tailSumNumbers 1000
+tailSumNumbers 10000
+tailSumNumbers 100000
+tailSumNumbers 1000000
+
+let fibonacci n =
+    let rec fibHelper n a b =
+        if n = 0 then a
+        elif n = 1 then b
+        else fibHelper (n - 1) b (a + b)
+    fibHelper n 0 1
+
+(*
+    +--------------------+---------------------+
+    |   ___              |                     |
+    |  /   \   NO        |                     |
+    |  | o o|            |   Stack overflow    |
+    |  \_^_/             |                     |
+    |                    |                     |
+    +--------------------+---------------------+
+    |   ___              |                     |
+    |  /   \   YES       |                     |
+    |  | ^ ^|            |  Integer overflow   |
+    |  \_^_/             |                     |
+    |                    |                     |
+    +--------------------+---------------------+
+*)
+fibonacci 100000
