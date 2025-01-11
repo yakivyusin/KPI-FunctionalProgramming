c1941e1a2879a0032fdac87a700eccebc54355c0
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-01-11 13:43:40 +0200

    Add Recursive Data Extra Sample

---
+++ Lct99-Extras/Topic2/RecursiveData.fsx
@@ -0,0 +1,16 @@
+﻿(* Краще дивитися після: лекція 5 *)
+
+(*
+    Крім рекурсивних функцій, ми можемо використовувати ключове слово rec для того,
+    щоб скомпілювати створення значень рекурсивних типів.
+*)
+type TreeNode = { Parent: TreeNode; Value: int }
+
+(*
+    Корінь дерева посилається на самого себе у якості батька, але без rec ми це не скомпілюємо.
+*)
+let rec root = { Parent = root; Value = 10 }
+let child = { Parent = root; Value = 20 }
+let child2 = { Parent = child; Value = 30 }
+
+printfn "%A" child2
