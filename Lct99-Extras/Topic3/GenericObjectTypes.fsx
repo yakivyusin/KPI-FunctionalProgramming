a3a53b6176299c155e371c256164e64433c55ae3
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-01-25 12:45:19 +0200

    Add Generic Class and Interface Extra Sample

---
+++ Lct99-Extras/Topic3/GenericObjectTypes.fsx
@@ -0,0 +1,20 @@
+﻿(* Краще дивитися після: лекція 11 *)
+
+(*
+    Ми можемо створювати узагальнені класи та інтерфейси, як і будь-які інші типи.
+*)
+
+type IGenericInterface<'T> =
+    abstract member Get: unit -> 'T
+    abstract member GetToString: unit -> string
+
+type MyClass<'T>(t: 'T) =
+    interface IGenericInterface<'T> with
+        member this.Get () = t
+        member this.GetToString () = t.ToString().ToUpper()
+
+let stringValue: IGenericInterface<string> = MyClass "Hello, World"
+stringValue.GetToString () |> printfn "%s"
+
+let intValue: IGenericInterface<int> = MyClass 1
+intValue.GetToString () |> printfn "%s"
