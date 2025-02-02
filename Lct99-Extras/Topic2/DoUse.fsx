798f99d819bae2febfdf5d4b3cb1a50acfd504d6
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-02-02 15:56:12 +0200

    Add Do and Use Bindings Sample

---
+++ Lct99-Extras/Topic2/DoUse.fsx
@@ -0,0 +1,28 @@
+﻿(* Краще дивитися після: лекція 2 *)
+
+(*
+    Крім let-прив'язки, у F# існують ще дві інші: do та use.
+*)
+
+(*
+    do використовується для виклику виразів, що повертають unit, без визначення символу.
+    По правильному, ми повинні завжди використовувати do у таких випадках, але
+    компілятор дозволяє у більшості випадків обійтися без нього.
+    Але є ситуації, коли do обов'язковий.
+*)
+printfn "Hello, World!"
+do printfn "Hello, World!"
+
+(*
+    Прив'язка use використовується для значень, що реалізовують інтерфейс IDisposable.
+    Тоді метод Dispose() буде викликано автоматично при завершенні поточної області видимості.
+*)
+let readFile file =
+    use stream = System.IO.File.Open (file, System.IO.FileMode.Open)
+    use reader = new System.IO.StreamReader (stream)
+    reader.ReadToEnd ()
+
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let self = readFile "DoUse.fsx"
+printfn "%s" self
