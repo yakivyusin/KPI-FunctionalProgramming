b8a0468202073cd4e54f858c0894231bf1c20665
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-03-15 17:00:33 +0200

    Add FsCheck Samples

---
+++ Lct15-PropertyBasedTesting/UsingFsCheck/AddTests.fs
@@ -0,0 +1,30 @@
+﻿module ``Add Tests``
+
+open FsCheck.Xunit
+
+let add x y = x + y
+
+[<Property(Verbose = true)>]
+let commutativeProperty x y =
+    let result1 = add x y
+    let result2 = add y x
+    result1 = result2
+
+(*
+    Змінюємо кількість тестів на 10000.
+*)
+[<Property(MaxTest = 10000)>]
+let identityProperty x =
+    let result = add x 0
+    result = x
+
+(*
+    Генератори значень у FsCheck отримують два параметри - кількість значень та параметр size.
+    Сенс size визначається типом значення - максимально допустиме значення числа, максимально допустима довжина колекції, тощо.
+    Всі властивості запускаються зі значеннями size, що лінійно збільшуються від StartSize (1) до EndSize (100).
+*)
+[<Property(StartSize = 100, EndSize = 1000)>]
+let associativeProperty x y z =
+    let result1 = add x (add y z)
+    let result2 = add (add x y) z
+    result1 = result2
---
+++ Lct15-PropertyBasedTesting/UsingFsCheck/EmployeeTests.fs
@@ -0,0 +1,38 @@
+﻿module ``Employee Tests``
+
+open FsCheck
+open FsCheck.Xunit
+open FsCheck.FSharp
+
+type Employee = { Name: string; Age: int }
+type Department = { Name: string; Team: Employee list }
+
+let isLargeDepartment department = department.Team.Length > 10
+let isLessThanTwenty person = person.Age < 20
+
+(*
+    Зазвичай FsCheck достатньо 20-30 тестів, щоб дійти до значень більше 20.
+*)
+[<Property>]
+let ``is Employee young for any age`` age =
+    isLessThanTwenty { Name = "Jane Smith"; Age = age }
+
+[<Property>]
+let ``is Employee young when age < 20`` age =
+    age < 20 ==> isLessThanTwenty { Name = "Jane Smith"; Age = age }
+
+(*
+    У попередньому тесті вираз праворуч від ==> обчислюється для будь-яких age, незважаючи на значення умови зліва.
+    Щоб запускати властивість тільки тоді, коли умова дійсно виконується, ми можемо використовувати вбудований lazy (System.Lazy).
+*)
+[<Property>]
+let ``is Department large when team > 10`` team =
+    (team |> List.length) > 10 ==> lazy isLargeDepartment { Name = "Super Department"; Team = team }
+
+(*
+    Всі вбудовані типи знаходяться у просторі імен FsCheck.
+    Оскільки всі вони є об'єднаннями з одним випадком, ми можемо їх деконструювати у параметрах.
+*)
+[<Property>]
+let ``is Employee young with PositiveInt`` (NonEmptyString name) (PositiveInt age) =
+    age < 20 ==> isLessThanTwenty { Name = name; Age = age }
