2292b2b21c95847a3a4754924a4f229ff6103b1f
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-03-16 14:14:06 +0200

    Add Introduction to PBT Sample

---
+++ Lct15-PropertyBasedTesting/Introduction/ModuleUnderTest.fs
@@ -0,0 +1,175 @@
+﻿module ModuleUnderTest
+
+(*
+    Не підглядати!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Не підглядати!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Не підглядати!
+
+*)
+let add x y =
+    if x = 1 && y = 1 then 2
+    else if x = 1 && y = 2 then 3
+    else if x = 1 && y = 3 then 4
+    else if x = 1 && y = 4 then 5
+    else if x = 1 && y = 5 then 6
+    else if x = 1 && y = 6 then 7
+    else if x = 1 && y = 7 then 8
+    else if x = 1 && y = 8 then 9
+    else if x = 1 && y = 9 then 10
+    else if x = 1 && y = 10 then 11
+    else if x = 2 && y = 1 then 3
+    else if x = 2 && y = 2 then 4
+    else if x = 2 && y = 3 then 5
+    else if x = 2 && y = 4 then 6
+    else if x = 2 && y = 5 then 7
+    else if x = 2 && y = 6 then 8
+    else if x = 2 && y = 7 then 9
+    else if x = 2 && y = 8 then 10
+    else if x = 2 && y = 9 then 11
+    else if x = 2 && y = 10 then 12
+    else if x = 3 && y = 1 then 4
+    else if x = 3 && y = 2 then 5
+    else if x = 3 && y = 3 then 6
+    else if x = 3 && y = 4 then 7
+    else if x = 3 && y = 5 then 8
+    else if x = 3 && y = 6 then 9
+    else if x = 3 && y = 7 then 10
+    else if x = 3 && y = 8 then 11
+    else if x = 3 && y = 9 then 12
+    else if x = 3 && y = 10 then 13
+    else if x = 4 && y = 1 then 5
+    else if x = 4 && y = 2 then 6
+    else if x = 4 && y = 3 then 7
+    else if x = 4 && y = 4 then 8
+    else if x = 4 && y = 5 then 9
+    else if x = 4 && y = 6 then 10
+    else if x = 4 && y = 7 then 11
+    else if x = 4 && y = 8 then 12
+    else if x = 4 && y = 9 then 13
+    else if x = 4 && y = 10 then 14
+    else if x = 5 && y = 1 then 6
+    else if x = 5 && y = 2 then 7
+    else if x = 5 && y = 3 then 8
+    else if x = 5 && y = 4 then 9
+    else if x = 5 && y = 5 then 10
+    else if x = 5 && y = 6 then 11
+    else if x = 5 && y = 7 then 12
+    else if x = 5 && y = 8 then 13
+    else if x = 5 && y = 9 then 14
+    else if x = 5 && y = 10 then 15
+    else if x = 6 && y = 1 then 7
+    else if x = 6 && y = 2 then 8
+    else if x = 6 && y = 3 then 9
+    else if x = 6 && y = 4 then 10
+    else if x = 6 && y = 5 then 11
+    else if x = 6 && y = 6 then 12
+    else if x = 6 && y = 7 then 13
+    else if x = 6 && y = 8 then 14
+    else if x = 6 && y = 9 then 15
+    else if x = 6 && y = 10 then 16
+    else if x = 7 && y = 1 then 8
+    else if x = 7 && y = 2 then 9
+    else if x = 7 && y = 3 then 10
+    else if x = 7 && y = 4 then 11
+    else if x = 7 && y = 5 then 12
+    else if x = 7 && y = 6 then 13
+    else if x = 7 && y = 7 then 14
+    else if x = 7 && y = 8 then 15
+    else if x = 7 && y = 9 then 16
+    else if x = 7 && y = 10 then 17
+    else if x = 8 && y = 1 then 9
+    else if x = 8 && y = 2 then 10
+    else if x = 8 && y = 3 then 11
+    else if x = 8 && y = 4 then 12
+    else if x = 8 && y = 5 then 13
+    else if x = 8 && y = 6 then 14
+    else if x = 8 && y = 7 then 15
+    else if x = 8 && y = 8 then 16
+    else if x = 8 && y = 9 then 17
+    else if x = 8 && y = 10 then 18
+    else if x = 9 && y = 1 then 10
+    else if x = 9 && y = 2 then 11
+    else if x = 9 && y = 3 then 12
+    else if x = 9 && y = 4 then 13
+    else if x = 9 && y = 5 then 14
+    else if x = 9 && y = 6 then 15
+    else if x = 9 && y = 7 then 16
+    else if x = 9 && y = 8 then 17
+    else if x = 9 && y = 9 then 18
+    else if x = 9 && y = 10 then 19
+    else if x = 10 && y = 1 then 11
+    else if x = 10 && y = 2 then 12
+    else if x = 10 && y = 3 then 13
+    else if x = 10 && y = 4 then 14
+    else if x = 10 && y = 5 then 15
+    else if x = 10 && y = 6 then 16
+    else if x = 10 && y = 7 then 17
+    else if x = 10 && y = 8 then 18
+    else if x = 10 && y = 9 then 19
+    else if x = 10 && y = 10 then 20
+    else 0
---
+++ Lct15-PropertyBasedTesting/Introduction/Tests.fs
@@ -0,0 +1,104 @@
+﻿module Tests
+
+open ModuleUnderTest
+open Xunit
+
+[<Fact>]
+let ``When I add 1 + 2, I expect 3`` () =
+    Assert.Equal(3, add 1 2)
+
+[<Fact>]
+let ``When I add 2 + 2, I expect 4`` () =
+    Assert.Equal(4, add 2 2)
+
+(*
+    Можемо перевірити ще декілька випадків через цикл у тілі тесту або за допомогою Theory.
+*)
+[<Theory>]
+[<InlineData(2, 3, 5)>]
+[<InlineData(5, 5, 10)>]
+[<InlineData(7, 7, 14)>]
+[<InlineData(8, 2, 10)>]
+[<InlineData(9, 9, 18)>]
+[<InlineData(10, 10, 20)>]
+let ``When I add two numbers, I expect their sum`` x y sum =
+    Assert.Equal(sum, add x y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let randomInt =
+    let r = System.Random ()
+    fun () -> r.Next 1000
+
+[<Fact>]
+let ``When I add two random numbers (100 times), I expect their sum`` () =
+    (*
+        Повторюємо 100 разів, щоб випадково не потрапити у діапазон 1..10 єдиним запуском.
+    *)
+    for _ = 1 to 100 do
+        let x = randomInt ()
+        let y = randomInt ()
+        Assert.Equal(x + y, add x y)
+
+(*
+    Як нам протестувати додавання без додавання? Згадати про його _властивості_.
+*)
+
+[<Fact>]
+let ``When I add two numbers (100 times), the result should not depend on parameter order`` () =
+    for _ = 1 to 100 do
+        let x = randomInt ()
+        let y = randomInt ()
+        Assert.Equal(add x y, add y x)
+
+[<Fact>]
+let ``When I add 0 to number (100 times), the result should be equal to this number`` () =
+    for _ = 1 to 100 do
+        let x = randomInt ()
+        Assert.Equal(x, add x 0)
+
+[<Fact>]
+let ``When I add three numbers (100 times), the result should not depend on adding order`` () =
+    for _ = 1 to 100 do
+        let x = randomInt ()
+        let y = randomInt ()
+        let z = randomInt ()
+        Assert.Equal(add x (add y z), add (add x y) z)
