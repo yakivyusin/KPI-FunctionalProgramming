653a8fbfa5effc85fcb84b7b6864b89e43f52d2a
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-04-16 11:31:10 +0300

    Update HtmlProvider Sample

--- Lct10-TypeProviders/TypeProviders/HtmlProvider.fsx
+++ Lct10-TypeProviders/TypeProviders/HtmlProvider.fsx
@@ -7,6 +7,6 @@ open FSharp.Data
 type WikiPage = HtmlProvider<"https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)">
 
 WikiPage.GetSample().Tables.Table.Rows
-|> Array.map (fun x -> (x.``Country/Territory``, x.``United Nations[15] - Estimate``))
+|> Array.map (fun x -> (x.``Country/Territory``, x.``United Nations[14] - Estimate``))
 |> Array.filter (fun (country, _) -> country <> "World")
 |> Array.iter (printfn "%A")
